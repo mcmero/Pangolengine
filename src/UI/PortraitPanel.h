1526ca10395d2b7b290098cba12f4465f1cad262
Marek Cmero <7799828+mcmero@users.noreply.github.com>
2025-04-22 14:23:27 +1000

    Simple NPC portrait panels

---
+++ src/UI/PortraitPanel.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "../TextureManager.h"
+#include "IComponent.h"
+#include "PanelHelper.h"
+#include "SDL3/SDL_pixels.h"
+#include "SDL3/SDL_rect.h"
+#include "SDL3/SDL_render.h"
+
+class PortraitPanel : public IComponent {
+public:
+  PortraitPanel(float xpos, float ypos, float width, float height,
+                float borderThickness, SDL_Color borderColour,
+                SDL_Color innerColour, const char *texturePath)
+      : borderRect(PanelHelper::getBorderRect(xpos, ypos, width, height,
+                                              borderThickness)),
+        innerRect(PanelHelper::getInnerRect(xpos, ypos, width, height)),
+        portraitRect({xpos, ypos, width, height}), borderColour(borderColour),
+        innerColour(innerColour) {
+    portraitTex = TextureManager::LoadTexture(texturePath);
+  }
+
+  void render(SDL_Renderer *renderer) override {
+    if (show) {
+      TextureManager::Panel(borderRect, innerRect, borderColour, innerColour);
+
+      SDL_FRect srcRect = {0, 0, portraitRect.w, portraitRect.h};
+      TextureManager::Draw(portraitTex, srcRect, portraitRect, SDL_FLIP_NONE);
+    }
+  }
+
+  void update(Interactable *interactable, Dialogue *dialogue) override {
+    if (interactable != nullptr && interactable->active) {
+      show = true;
+    } else
+      show = false;
+  }
+
+private:
+  bool show = false;
+
+  SDL_FRect borderRect;
+  SDL_FRect innerRect;
+  SDL_FRect portraitRect;
+  SDL_Color borderColour;
+  SDL_Color innerColour;
+
+  SDL_Texture *portraitTex;
+};
--- src/UI/UIManager.h
+++ src/UI/UIManager.h
@@ -4,22 +4,21 @@
 #include "DialoguePanel.h"
 #include "DialogueResponsePanel.h"
 #include "Grid.h"
+#include "PortraitPanel.h"
 #include "SDL3/SDL_events.h"
+#include "SDL3/SDL_filesystem.h"
 #include "SDL3/SDL_render.h"
 #include <memory>
 
 class UIManager {
 public:
-  float pointsize = 14.0f;
-  SDL_Color fontColour = {255, 255, 255};
-  SDL_Color selectColour = {208, 199, 125};
-  SDL_Color dialogueBorderColour = {0, 0, 0};
-  SDL_Color dialogueBoxColour = {51, 64, 113};
-
   UIManager() {
     grid.addChild(std::make_shared<DialoguePanel>(
-        80.0f, 130.0f, 220.0f, 40.0f, 2.0f, dialogueBorderColour,
+        80.0f, 130.0f, 220.0f, 36.0f, 2.0f, dialogueBorderColour,
         dialogueBoxColour, pointsize, fontColour));
+    grid.addChild(std::make_shared<PortraitPanel>(
+        36.0f, 130.0f, 36.0f, 36.0f, 2.0f, dialogueBorderColour,
+        dialogueBoxColour, npcPortrait.c_str()));
     grid.addChild(std::make_shared<DialogueResponsePanel>(
         80.0f, 10.0f, 220.0f, 40.0f, 2.0f, dialogueBorderColour,
         dialogueBoxColour, pointsize, fontColour, selectColour));
@@ -34,4 +33,14 @@ public:
 
 private:
   Grid grid;
+
+  float pointsize = 14.0f;
+  SDL_Color fontColour = {255, 255, 255};
+  SDL_Color selectColour = {208, 199, 125};
+  SDL_Color dialogueBorderColour = {0, 0, 0};
+  SDL_Color dialogueBoxColour = {51, 64, 113};
+
+  fs::path assetsPath = fs::path(SDL_GetBasePath()) / "assets";
+  std::string npcPortrait =
+      (assetsPath / "portraits" / "npc_portrait.png").string();
 };
