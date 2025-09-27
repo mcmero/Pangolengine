6f986b94607ce0cc356588f282739eafabbadab6
Marek Cmero <7799828+mcmero@users.noreply.github.com>
2025-09-27 21:28:43 +1000

    Rough structure of ECS

--- src/Components/ECS.h
+++ src/Components/ECS.h
@@ -1,14 +1,61 @@
 #pragma once
 
-class IComponent;
-class Entity;
+#include <bitset>
+#include <memory>
+#include <array>
+#include <unordered_map>
+#include <vector>
+
+constexpr std::size_t maxComponents = 32;
 
 class IComponent {
 public:
-	Entity* entity;
-
-	virtual void init() {}
 	virtual void update() {}
 	virtual void render() {}
   virtual ~IComponent() = default;
+};
+
+using EntityId = std::size_t;
+
+struct Entity {
+  std::array<std::unique_ptr<IComponent>, maxComponents> componentArray;
+  std::bitset<maxComponents> componentBitset;
+};
+
+class EntityRegistry {
+public:
+  EntityRegistry() {};
+
+  /*
+   * Add an entity to the manager and return its ID
+   */
+  EntityId create() {};
+
+  /*
+   * Add component to entity and initialise
+   */
+  template<typename T, typename... TArgs>
+  T& addComponent(EntityId entityId, TArgs&&... mArgs) {};
+
+  /*
+   * Get all components of type T and return vector of entity IDs
+   * that have those components.
+   */
+  template<typename T, typename... TArgs>
+  std::vector<EntityId> getEntitiesWithComponents() {}
+
+  /*
+   * Remove the entity from the registry.
+   */
+  void remove(Entity &entity) {}
+
+  /*
+  * Clear all entities in the registry
+  */
+  void clear() {};
+
+  ~EntityRegistry() = default;
+
+private:
+  std::unordered_map<EntityId, Entity> entityMap;
 };
