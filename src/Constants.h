a9e0a84a2b67dbb3279be7c38606aee867b3eba5
Marek Cmero <7799828+mcmero@users.noreply.github.com>
2025-10-06 22:55:30 +1100

    Remove components from memory on destruction and fix map destructor causing issues

--- src/Constants.h
+++ src/Constants.h
@@ -7,6 +7,6 @@ static const int SCREEN_HEIGHT = 180;
 static const int DEFAULT_RENDER_SCALE = 4;
 static const int TILE_SIZE = 16;
 static const float PLAYER_SPEED = 50.0f; // pixels per second
-static const bool RENDER_COLLIDERS = false;
+static const bool RENDER_COLLIDERS = true;
 
 static const std::string ENTRY_MAP = "level1.tmj";
